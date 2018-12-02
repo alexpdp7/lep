9f5ceddd7df29668eb2acd4b52c1138b9adaed01
alex <alex@pdp7.net>
2018-12-02 11:17:51 +0100

    Add web

---
+++ lep_web.py
@@ -0,0 +1,12 @@
+import flask
+
+import lep_parser
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    articles = sorted(lep_parser.get_front_page_articles(), key=lambda a: a.section)
+    return flask.render_template('index.html', articles=articles)
