a16577e20fdd1e629ac7b832e63c9e82373bc8d0
alex <alex@pdp7.net>
2019-01-06 19:01:47 +0100

    Make deployable via Heroku and similar

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: env FLASK_APP=lep_web.py flask run --port=$PORT --host=0.0.0.0
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'lep',
+    packages = find_packages(),
+    install_requires = ['Flask', 'pyquery'],
+)
