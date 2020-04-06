1e43c21f038c19545fdc408154230a93a6c9ad6f
alex <alex@pdp7.net>
2020-04-06 14:04:41 +0200

    Apply black and ensure flake8 passes

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="lep", packages=find_packages(), install_requires=["Flask"],
-)
+setup(name="lep", packages=find_packages(), install_requires=["Flask"])
